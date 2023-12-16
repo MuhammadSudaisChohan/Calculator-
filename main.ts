c32b5881d28e8d34a7a76c717a7659bcfb9f4df5
MuhammadSudaisChohan <150429149+MuhammadSudaisChohan@users.noreply.github.com>
2023-12-16 05:45:33 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+let answer =await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberone",
+        message: "Enter your first number",
+    },
+    {
+        type: "number",
+        name: "numbertwo",
+        message: "Enter your second number",
+    },
+    {
+        type: "list",
+        name: "select",
+        choices: ["+", "-", "*", "/"]
+    }
+]);
+
+let numberone = answer.numberone;
+let numbertwo = answer.numbertwo;
+let result = 0;
+
+if (answer.select === "+") {
+    result = numberone + numbertwo;
+}
+else if (answer.select === "-") {
+    result = numberone - numbertwo;
+}
+else if (answer.select === "*") {
+    result = numberone * numbertwo;
+}
+else if (answer.select === "/") {
+    result = numberone / numbertwo;
+}
+
+console.log("Your result is", result);
+
